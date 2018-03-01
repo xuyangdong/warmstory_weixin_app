a928a6170e6d2c2c92be8646cf3b4958c11bc100
zs <806949096@qq.com>
2018-03-01 17:56:32 +0800

    add todayStory components
    Signed-off-by: zs <806949096@qq.com>

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
+
 const baseUrl = 'http://47.93.242.215'
 
 export default {
   user: {
     get: `${baseUrl}/wx/mockLogin?id=1066`
+  },
+  todayStory: {
+    getTodayStory: `${baseUrl}/wx/mockLogin?id=1066`
   }
 }
