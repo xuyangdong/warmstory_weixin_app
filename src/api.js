298ef6200e5ccbc7199a52cd8d02b2b551bfd0f4
xuyangdong@baidu.com <xuyangdong@baidu.com>
2018-03-01 18:48:45 +0800

    temp

--- src/api.js
+++ src/api.js
@@ -3,5 +3,8 @@ const baseUrl = 'http://47.93.242.215'
 export default {
   user: {
     get: `${baseUrl}/wx/mockLogin?id=1066`
+  },
+  ossToken: {
+    get: `${baseUrl}/auth/getOssSignature`
   }
 }
