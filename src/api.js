b73cac79331631f733b93eec69724b8caf777bcd
Laimzs <806949096@qq.com>
2018-03-08 13:35:54 +0800

    Signed-off-by: Laimzs <806949096@qq.com> fix

--- src/api.js
+++ src/api.js
@@ -6,9 +6,17 @@ export default {
     get: (id = 1066) => `${baseUrl}/wx/mockLogin?id=${id}`
   },
   todayStory: {
-    get: (id = 40) => `${baseUrl}/user/getStoryById?id=${id}`
+    get: `${baseUrl}/user/getRandomStoryList`
   },
   ossToken: {
     get: `${baseUrl}/auth/getOssSignature`
+  },
+  storyList: {
+    get: (playList = -1, page = 0, pageSize = 30) =>
+      `${baseUrl}/user/playList/getWorksListByPlayListIdByPage?playList=${playList}&page=${page}&pageSize=${pageSize}`
+  },
+  storySet: {
+    get: (page = 0, pageSize = 30) =>
+      `${baseUrl}/user/playList/getPlayListsByPage?page=${page}&pageSize=${pageSize}`
   }
 }
